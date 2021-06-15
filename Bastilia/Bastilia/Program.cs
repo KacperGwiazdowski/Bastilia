3e255221c6bfbdb722e0c808b74b2b4c583b9af1
Kacper GWIAZDOWSKI <kacper.gwiazdowski@axadirect-solutions.pl>
2021-06-15 15:41:27 +0200

    initial commit

---
+++ Bastilia/Bastilia/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bastilia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
